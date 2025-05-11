5d7b87c8fd354c3c630520cc39eced700d25031b
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-05-11 23:50:29 +0200

    feat: add local middelware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,29 @@
 <?php
 
+use App\Http\Middleware\LocalizationMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'loc' => LocalizationMiddleware::class,
+
+        ]);
+        $middleware->web(remove: [
+            // SubstituteBindings::class,
+        ]);
+        $middleware->web(append: [
+                // \CodeZero\LocalizedRoutes\Middleware\SetLocale::class,
+                // SubstituteBindings::class,
+            LocalizationMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
