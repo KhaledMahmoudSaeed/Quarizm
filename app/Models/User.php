275c98d08d5cce89bbef7bb89779d1c1c2e26d0e
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-05-11 23:49:55 +0200

    fix: add some relation methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -54,6 +55,10 @@ class User extends Authenticatable
     public function workshops(): HasMany
     {
         return $this->hasMany(Workshop::class);
+    }
+    public function coachData(): HasOne
+    {
+        return $this->hasOne(CoachDate::class);
     }
     public function privateSession(): HasMany
     {
--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Workshop extends Model
 {
@@ -26,5 +27,9 @@ class Workshop extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
